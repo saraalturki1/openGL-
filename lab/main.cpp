6a6b79b93873e2aa3df71e1abb8f468b9b33fe15
SARA ALTURKI <saraalturki37@gmail.com>
2022-08-15 13:47:54 +0300

    Initial Commit

---
+++ lab/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  lab
+//
+//  Created by Aleen on 17/01/1444 AH.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
